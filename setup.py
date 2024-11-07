7807df12f3129752f6b704ba86df8227c6c36d38
Francesco Baldi <francesco.baldi86@gmail.com>
2024-11-07 16:14:27 +0100

    Modified list of required packages

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/discussions/install-requires-vs-requirements/
     install_requires=[
-        'pandas', 'yaml', 'os', 'requests', 'json', 'schedule', 'time'
+        'pandas', 'pyaml', 'os', 'requests', 'schedule'
     ],  # Optional, 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
